b3c2989a0fc2673a4cc6904dee2a18a89b468f76
alandrnov <al.andr.nov@gmail.com>
2021-07-23 16:35:07 +0300

    add File

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String messag;
+
+    public String getMessage() {
+        return messag;
+    }
+
+    public void setMessage(String message) {
+        this.messag = message;
+    }
+}
